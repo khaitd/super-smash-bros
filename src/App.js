3ba2434bd0bf3f86e8fb1f516637887c7222a6d7
Dmitriy <khaitdmitriy@gmail.com>
2022-04-05 12:23:23 -0400

    added figter js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import './App.css';
+import Fighter from './components/Fighter';
 
 function App() {
   return (
     <div className="App">
       <h1>  Fighters</h1>
       <div className="fighters-grid" >
-        
-      </div>
-      
+          <Fighter name={'Roy'} color={'red'} />
+          <Fighter name={'Fox'} color={'green'} />
+          <Fighter name={'Ganondorf'} color={'purple'}/>
+      </div>    
     </div>
   );
 }
