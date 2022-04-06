55f039f0eca911b9a554e1352a3f2e8fbcadf8d0
Dmitriy <khaitdmitriy@gmail.com>
2022-04-06 11:05:57 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -6,25 +6,35 @@ import {useState} from 'react';
 
 
 const characters = [
-  {name: 'Roy', color: 'red'},
-  {name: 'Fox', color: 'green'},
-  {name: 'Ganondorf', color: 'purple'},
-  {name: 'Sheik', color: 'lightskyblue'}
+  {name: "Roy",color: "red"},
+  {name: "Fox", color: "green"},
+  {name: "Ganondorf", color: "purple"},
+  {name: "Sheik", color: "lightskyblue"},
+  {name: "Mario", color: "silver"},
+  {name: "Yoshi", color: "lightskyblue"},
+  {name: "Bowser", color: "blue"},
+  {name: "Link", color: "lightskyblue"},
+  {name: "jigglypuff", color: "lightskyblue"},
+  {name: "Pikachu", color: "orange"}
 ]
+
 
 function App() {
   const [visible, setVisible] = useState(false)
+  const [selectCharacter, setSelectedCharacter ]  = useState(characters[0]);
+
   return (
     <div className="App">
       <h1>  Fighters</h1>
       <div className="fighters-grid" >
 
         {characters.map((element,i) => {
-           return( <Fighter name={element.name} color={element.color} setVisible={setVisible}  />)
-        })}
+           return (<Fighter name={element.name} color={element.color} setVisible={setVisible} selectCharacter={selectCharacter} setSelectedCharacter={setSelectedCharacter}/>)
+        })
+        }
          
       </div>    
-        <FighterScreen isVisible={visible} setVisible={setVisible}></FighterScreen>
+        <FighterScreen isVisible={visible} setVisible={setVisible} selectCharacter={selectCharacter} />
 
 
     </div>
--- src/components/FighterScreen.js
+++ src/components/FighterScreen.js
@@ -1,8 +1,11 @@
-const FighterScreen = ({isVisible, setVisible}) => {
+import { selectOptions } from "@testing-library/user-event/dist/select-options";
+
+const FighterScreen = ({isVisible, setVisible, selectCharacter}) => {
     return(
         <div onClick={() => {setVisible(false)}} className="fighter-screen" style={{display: isVisible ? "block" : "none"}}>
-          <h2>Roy</h2>
-            <img width="100" height="100" src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png"></img>
+          <span>{selectOptions.name}</span>
+          <h2>{selectCharacter.name}</h2>
+            <img characterName="char-portrait" width="100" height="100" src={`https://www.smashbros.com/assets_v2/img/fighter/${selectCharacter.name.toLowerCase()}/main.png`}></img>
             <p>
             sdakjfksdjfl;kaj;lk asd;fja;ksdhfkasjkj asd;lkja;ksdhfkjsdhfjkhd a;sdhfakljsdhfjkahds ashdfkjahsdfkj
             </p>
